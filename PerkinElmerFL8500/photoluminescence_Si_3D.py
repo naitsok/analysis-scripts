31103cf4c8bbac0ff81ce878a316cb36943da1ef
naitsok <contact@ktamarov.com>
2021-12-03 17:42:16 +0200

    Finalized script for analysis of PerkinElmer 3D data. ReadMe is pending.

--- PerkinElmerFL8500/photoluminescence_Si_3D.py
+++ PerkinElmerFL8500/photoluminescence_Si_3D.py
@@ -24,6 +24,7 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import xlsxwriter
 
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
@@ -53,12 +54,12 @@ def load_csv(meas_folder: str, encoding: str) -> pd.DataFrame:
     return meas_df
 
 
-def analyze_sample(measure_dir: str,
+def get_sample_data(measure_dir: str,
                    sample_id: str,
                    emission_filters: list,
                    excitation_wavelengths: list,
                    encoding: str) -> list:
-    '''Analyze one sample with sample_id, excitation wavelengths and emission filters'''
+    '''Get sample data for sample with sample_id, excitation wavelengths and emission filters'''
     # get all folders with specified
     all_sample_paths = [x for x in glob.glob(os.path.join(measure_dir, sample_id + '*')) if os.path.isdir(x)]
     print(all_sample_paths)
@@ -108,14 +109,66 @@ def analyze_sample(measure_dir: str,
         sample_excit_wls = np.array([float(x.strip(')').strip('(')) for x in list(meas_df.columns[1:])])
         sample_data = meas_df.iloc[:,1:].to_numpy()
         
-    # create new dataset and save it as csv
-    export_df = pd.DataFrame(data=np.concatenate((x_nm[:, None], sample_data), axis=1),
-                             columns=['nm'] + sample_excit_wls.tolist())
-    export_df.to_csv(os.path.join(measure_dir, sample_id + '_filters_' + '_'.join(emission_filters) + '_nm.csv'),
-                     index=False)
+    
         
     return [x_nm, sample_excit_wls, sample_data]
 
+def analyze_sample(args: argparse.Namespace):
+    '''Performs analysis of the sample spectrum: subtracts background if specified and gets 
+    values for the peak emission.'''
+    x_nm, excit_wls, spectra = get_sample_data(args.measure_dir, args.sample_id, args.emission_filters,
+                                               args.excitation_wavelengths, args.encoding)
+    if args.bg_id:
+        bg_x_nm, bg_excit_wls, bg_spectra = get_sample_data(args.measure_dir, 
+                                                            args.bg_id, 
+                                                            args.emission_filters,
+                                                            args.excitation_wavelengths,
+                                                            args.encoding)
+        # manipulate the bg values for each excitation wavelengths
+        # this is needed becuase background peak at ~830 nm is much larger 
+        # then when sample is in holder and thus gives negative values.
+        bg_max_idx = np.argmax(bg_spectra, axis=0)
+        bg_max = np.amax(bg_spectra, axis=0)
+        spectra_vals = spectra[bg_max_idx].diagonal()
+        # multiply background to avoid negative values
+        corr_factors = spectra_vals / bg_max
+        bg_spectra = bg_spectra * corr_factors[None, :] * args.correction_factor
+        
+        # subtract the background from sample spectra
+        spectra = spectra - bg_spectra
+    
+    # get the peak value of PL and the emission wavelength for each excitation wavelength
+    # select data with emission wavelngth below 810 nm due to background peak at ~830 nm
+    sample_data810 = spectra[x_nm <= 810, :]
+    emission_max = np.amax(sample_data810, axis=0)
+    # normalize to emission max
+    spectra = spectra / np.amax(emission_max)
+    emission_max = emission_max / np.amax(emission_max)
+    wavelength_max = x_nm[np.argmax(sample_data810, axis=0)]
+    # normalize spectra
+    max_from_emission = np.stack((excit_wls, wavelength_max, emission_max), axis=1)
+        
+    # exporting to Excel to be further used for importing into OriginPro
+    xlsx_file = os.path.join(args.measure_dir, args.sample_id + '_filters_' + '_'.join(args.emission_filters) + '_nm.xlsx')
+    with xlsxwriter.Workbook(xlsx_file) as wb:
+        wsh = wb.add_worksheet(args.sample_id)
+        # first write column title
+        wsh.write_row(0, 0, ['Emission wavelength'] + ['PL intensity'] * len(excit_wls) + 
+                      ['Emission wavelength', 'Peak emission wavelength', 'Peak PL intensity'])
+        # second write column units 
+        wsh.write_row(1, 0, ['nm'] + ['a.u.'] * len(excit_wls) + ['nm', 'nm', 'a.u.'])
+        # third, write comments
+        wsh.write_row(2, 0, [''] + [f'{args.sample_id} {x} nm' for x in excit_wls] + ['', args.sample_id, args.sample_id])
+        # write the x nm column for 3D spectra
+        wsh.write_column(3, 0, x_nm)
+        # write 3D spectral data
+        for col in range(spectra.shape[1]):
+            wsh.write_column(3, col + 1, spectra[:, col])
+        # write the PL maximum changes
+        for col in range(max_from_emission.shape[1]):
+            wsh.write_column(3, col + spectra.shape[1] + 1, max_from_emission[:, col])
+
+    return [x_nm, excit_wls, spectra]
  
 class PhotoluminescenceSi3DParser(argparse.ArgumentParser):
     '''Class to perform parsing the input arguments and do additional checks of the input data.'''
@@ -141,15 +194,17 @@ class PhotoluminescenceSi3DParser(argparse.ArgumentParser):
             self.error('sample id must be specified when not full dir is analyzed')
             
         if args.sample_id:
-            x_nm, excit_wls, spectra = analyze_sample(args.measure_dir, args.sample_id, args.emission_filters, 
-                                                      args.excitation_wavelengths, args.encoding)
+            x_nm, excit_wls, spectra = analyze_sample(args)
             x_nm, excit_wls = np.meshgrid(x_nm, excit_wls)
 
-            fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+            fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
 
             # Plot the surface.
             surf = ax.plot_surface(x_nm, excit_wls, spectra.T, cmap=cm.coolwarm,
                                 linewidth=0, antialiased=False)
+            ax.set_xlabel('Emissition wavelength (nm)')
+            ax.set_ylabel('Excitation wavelength (nm)')
+            ax.set_zlabel('Intensity (a.u.)')
 
             # Customize the z axis.
             # ax.set_zlim(-1.01, 1.01)
@@ -171,22 +226,27 @@ parser = PhotoluminescenceSi3DParser(description='''Analysis of 3D photoluminesc
                                      at differect excitation wavelengths and different emission filters.''')
 parser.add_argument('measure_dir', metavar='measure_dir', type=str,
                     help='Directory where folders with measurements are located.')
-parser.add_argument('-sa', '--sample-id', type=str, default='',
+parser.add_argument('-si', '--sample-id', type=str, default='',
                     help='''Sample id, that is at the beginning of folder with measurement for this sample. Not needed
                     when whole directory with measurements is analyze. Default: empty string.''')
+parser.add_argument('-bg', '--bg-id', type=str, default='',
+                    help='''Background id, background file must contain measruements with the same emission filters as
+                    sample)s_.''')
 parser.add_argument('-ad', '--analyze-dir', action='store_true',
                     help='''If present, script analyzes the whole directory with measurment and does not plot results. 
                     The repeat number is automatically taken to be the latest one.''')
 parser.add_argument('-en', '--encoding', type=str, default='',
                     help='''Endofing for the spectra CSV file. If empty, script tries to detect encoding automatically. Default: "".''')
-parser.add_argument('-ew', '--excitation-wavelengths', type=str, default='300-410, 410-500',
+parser.add_argument('-ew', '--excitation-wavelengths', type=str, default='300-410, 410-490',
                     help='''The excitation wavelength ranges that indicate the range for which a specific emission filter is applied to.
-                    Default: "300-400, 400-500". The default values indicates two ranges of excitation wavelengths which are 300 to 400 nm, 
+                    Default: "300-400, 400-490". The default values indicates two ranges of excitation wavelengths which are 300 to 400 nm, 
                     and 400 to 500 nm.''')
 parser.add_argument('-ef', '--emission-filters', type=str, default='430, 515',
                     help='''The emission filters in nm that were applied to each range of excitation wavelength. The number of
                     filters must match to the number of ranges. These filter values will be searched in the folder with 
                     measurement. Default: "430, 515". ''')
+parser.add_argument('-cf', '--correction-factor', type=float, default=0.9,
+                    help='''Correction factor to multiply background spectrum for correction of sample spectrum.''')
 
 
 if __name__ == '__main__':
